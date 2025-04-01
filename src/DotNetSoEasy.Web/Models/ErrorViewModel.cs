2320de8771029fef3aa6be848144780ca1f3042f
Jayro.Liu <Jayro.Liu@outlook.com>
2025-04-02 01:44:43 +0800

    简洁架构

---
+++ src/DotNetSoEasy.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetSoEasy.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
